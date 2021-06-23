ae8f82133d1e0225a652ee5c30dfe2bdcc10bd70
sergei-okon <sergei.okon@gmail.com>
2021-06-23 22:57:29 +0300

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,41 @@
 package day19;
 
-/**
- * @author Neil Alishev
- */
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+
+        countMaxWords();
+    }
+
+    static public void countMaxWords() throws IOException {
+        FileInputStream stream = new FileInputStream("C:\\Java\\AlishevMarathon\\JavaMarathon2021\\src\\main\\resources\\dushi.txt");
+        int lenght = stream.available();
+        byte[] data = new byte[lenght];
+        stream.read(data);
+
+        String text = new String(data);
+        String[] words = text.split("[.,:;()?!\"\\s–]+");
+
+        Map<String, Long> wordsCount = new HashMap<>();
+
+        for (String word : words) {
+            long countOfWord = wordsCount.get(word) == null ? 0 : wordsCount.get(word);
+            wordsCount.put(word, countOfWord + 1);
+        }
+        System.out.println(wordsCount.get("Чичиков"));
+
+        Map<String, Long> top100 = new LinkedHashMap<>();
+        wordsCount.entrySet().stream()
+                .sorted((x1, x2) -> Long.compare(x2.getValue(), x1.getValue()))
+                .limit(100)
+                .forEach(x -> top100.put(x.getKey(), x.getValue()));
+
+        System.out.print(top100);
     }
 }
+
--- src/main/java/day19/Task2.java
+++ src/main/java/day19/Task2.java
@@ -1,10 +1,61 @@
 package day19;
 
-/**
- * @author Neil Alishev
- */
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Java\\AlishevMarathon\\JavaMarathon2021\\src\\main\\resources\\numbers19.txt");
+
+        numbersArrayList(file);
+
+        numbersHashMap(file);
+    }
+
+    static public void numbersArrayList(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            List<Integer> numeric = new ArrayList<>();
+            int counter = 0;
+
+            while (scanner.hasNext()) {
+                numeric.add(scanner.nextInt());
+            }
+
+            for (Integer a : numeric) {
+                if (a > 500_000_000 && a < 600_000_000)
+                    counter++;
+            }
+            System.out.println("Кол-во чисел " + counter);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+    }
+
+    static public void numbersHashMap(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            Map<Integer, Integer> num = new HashMap<>();
+            int count = 0;
+
+            while (scanner.hasNext()) {
+                num.put(scanner.nextInt(), 0);
+            }
+            for (Map.Entry<Integer, Integer> entry : num.entrySet()) {
+                if (entry.getKey() > 500_000_000 && entry.getKey() < 600_000_000) {
+                    count++;
+                }
+            }
+            System.out.println("Кол-во чисел " + count);
+            System.out.println(num);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
+
+
